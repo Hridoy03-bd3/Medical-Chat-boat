5ffdc04f441659c14b9a666a47e350658b0f45ea
Your Name <your.email@example.com>
2026-09-15 11:13:59 +0600

    done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Hridoy Ahmed',
+    author_email= 'hridoyahmed9197@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
